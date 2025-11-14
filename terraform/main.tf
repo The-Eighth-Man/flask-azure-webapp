379c5b724ca3756b339617e51ec0861ec5cd5689
Mateo Durán <mduran@prioritypower.com>
2025-11-14 11:03:52 -0500

    Add random suffix to resource group name for uniqueness

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,7 +56,7 @@ resource "random_string" "suffix" {
 
 # Resource Group
 resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
+  name     = "${var.resource_group_name}-${random_string.suffix.result}"
   location = var.location
 
   tags = {
